55a458bebf603efc7c12cbfe180eb3afc63d36c9
junhyek <junhyek@coblic.com>
2018-12-31 12:17:19 +0900

    feat: global style

--- src/global/styles/index.js
+++
@@ -1,5 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-const constants = {
-    padding: 10,
-}
---
+++ src/styles/index.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+const coblicButton = {
+};
+
+export default StyleSheet.create({
+    coblicBlueButton: { ...coblicButton,
+        color: '#0052f3'
+    },
+    coblicRedButton: { ...coblicButton,
+        color: '#da5f6e'
+    },
+    RISE: {
+        color: '#da5f6e'
+    },
+    FALL: {
+        color: '#0052f3'
+    },
+    BUY: {
+        color: '#da5f6e'
+    },
+    SELL: {
+        color: '#0052f3'
+    },
+});
