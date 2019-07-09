4fc60676d561f4851285cd2531a95769b0ef53c2
junhyek <junhyek@coblic.com>
2019-07-09 19:22:03 +0900

    does not affect by phone setting

--- src/components/ChartBox/index.js
+++ src/components/ChartBox/index.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, WebView } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { inject, observer } from 'mobx-react';
+import { WebView } from 'react-native-webview';
 
 @inject('tradingPairStore')
 @observer
 export default class ChartBox extends Component {
     render() {
-        const { selectedTradingPair } = this.props.tradingPairStore || {};
-        const INJECTEDJAVASCRIPT = `const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `
+        const {
+            selectedTradingPair
+        } = this.props.tradingPairStore || {};
         
         return (
             <View style={styles.container}>
                 <WebView
-                    ref={(view) => this.webView = view}
+                    textZoom={100}
                     originWhitelist={['*']}
                     source={{
                         uri: `${Expo.Constants.manifest.extra.REACT_WEB_API_ENDPOINT}/chart-view/${selectedTradingPair ? selectedTradingPair.name : ''}`,
                     }}
-
-                    injectedJavaScript={INJECTEDJAVASCRIPT}
                     javaScriptEnabledAndroid={true}
                     javaScriptEnabled={true}
                 />
