1e5371083e452cb975cefa65eb4b7a0957ae2f1f
junhyek <junhyek@coblic.com>
2019-07-22 00:18:21 +0900

    load on open screen

--- src/components/AssetsAndEvaluationBox/index.js
+++ src/components/AssetsAndEvaluationBox/index.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import TotalAssetsEvaluation from './TotalAssetsEvaluation';
 import AssetsAndEvaluationList from './AssetsAndEvaluationList';
-import { observer } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import commonStyle from '../../styles/commonStyle';
 
+@inject('userStore', 'accountStore')
 @observer
 export default class AssetsAndEvaluationBox extends Component {
-  render() {
-    return (
-      <View style={ styles.container }>
-        <TotalAssetsEvaluation />
-        <AssetsAndEvaluationList />
-      </View>
-    )
-  }
+    componentDidMount() {
+        if (this.props.userStore.isLoggedIn) {
+            this.props.accountStore.loadAccounts();
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TotalAssetsEvaluation />
+                <AssetsAndEvaluationList />
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
